03d50b35d63fe76d537ddebb3fffc677b70a29b4
GeekyHacks <geekyhacks22@gmail.com>
2023-02-18 02:48:27 +0300

    push

--- compareTriplets.js
+++ compareTriplets.js
@@ -1,3 +1,35 @@
+
+//  accurate solution
+function compareTriplets(a, b) {
+  let aCounter = 0;
+  let bCounter = 0;
+  let outputArr = [];
+
+  if (a[0] > b[0]) {
+    aCounter++;
+  } else if (a[0] < b[0]) {
+    bCounter++;
+  }
+
+  if (a[1] > b[1]) {
+    aCounter++;
+  } else if (a[1] < b[1]) {
+    bCounter++;
+  }
+  if (a[2] > b[2]) {
+    aCounter++;
+  } else if (a[2] < b[2]) {
+    bCounter++;
+  }
+
+  outputArr.push(aCounter, bCounter);
+
+  return outputArr;
+}
+compareTriplets([17, 28, 30], [99, 16, 8]);
+
+
+// some wrong solutions
 function compareTriplets(a, b) {
   // let arra = a[0];
   // let arrb = b[0];
@@ -6,20 +38,27 @@ function compareTriplets(a, b) {
   let outputArr = [];
 
   for (let i = 0; i < a.length; i++) {
-    if (a[i] > b[i]) {
-      aCounter++;
-      outputArr.push(aCounter);
-    }
-    if (a[i] < b[i]) {
-      bCounter++;
-      outputArr.push(bCounter);
-    } else if (a[i] === b[i]) {
-      aCounter--;
-      bCounter--;
+    for (let j = 0; j < b.length; j++) {
+      if (a[i] !== b[j]) {
+        if (a[i] > b[j]) {
+          aCounter++;
+          bCounter--;
+          outputArr.push(aCounter);
+        }
+        if (a[i] < b[j]) {
+          aCounter--;
+          bCounter++;
+          outputArr.push(bCounter);
+        }
+      }
+      if (a[i] === b[j]) {
+        bCounter == aCounter;
+        outputArr.push(aCounter);
+        outputArr.push(bCounter);
+      }
     }
   }
 
   console.log(outputArr);
   return outputArr;
 }
-compareTriplets([5, 6, 7], [3, 6, 10]);
